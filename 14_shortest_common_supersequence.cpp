2cb9532a9be8813eb45f3deba33efe81e18da94f
Akashbhoi <akashkb@iitk.ac.in>
2022-01-25 18:04:01 +0530

    Length of Shortest Common Sub Sequence

--- 14_shortest_common_supersequence.cpp
+++ 14_shortest_common_supersequence.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
     string a, b;
@@ -10,5 +9,24 @@ int main()
     int m = a.size();
     int n = b.size();
 
-    
+    // LCS will appear once. L1 + L2 - LCS()
+
+    int dp[m + 1][n + 1];
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = 0;
+    for (int j = 0; j <= n; j++)
+        dp[0][j] = 0;
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (a[i - 1] == b[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    cout << m + n - dp[m][n];
 }
--- Readme.md
+++ Readme.md
@@ -1,4 +1,3 @@
 # dp
 [template](./_.cpp) <br />
 This covers all the problems discussed in [Aditya Verma](https://www.youtube.com/c/AdityaVermaTheProgrammingLord/featured) Dp series. Kindly refere his Youtube videos. It is really helpfull.
-
